7048573e3590311f60095314cd6ee91b003422c6
Michael Ngo <michaelngo1337@yahoo.com>
2023-03-09 14:51:53 -0800

    changed javascript injection to be semantic

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const displayData = jsonData => {
   document.getElementById("displayChart")
     .innerHTML += `
-      <div class="first-bar">
+      <article class="first-bar">
         <div>
-          <img src=${jsonData[0].icon} alt="">
+          <img src=${jsonData[0].icon} alt=${jsonData[0].category}>
           <p>${jsonData[0].category}</p>
         </div>
-        <p>${jsonData[0].score} / 100</p>
-      </div>
-      <div class="second-bar">
+        <p><span class="bold">${jsonData[0].score}</span> <span class="light-navy">/ 100</span></p>
+      </article>
+      <article class="second-bar">
         <div>
-          <img src=${jsonData[1].icon} alt="">
+          <img src=${jsonData[1].icon} alt=${jsonData[1].category}>
           <p>${jsonData[1].category}</p>
         </div>  
-        <p>${jsonData[1].score} / 100</p>
-      </div>
-      <div class="third-bar">
+        <p><span class="bold">${jsonData[1].score}</span> <span class="light-navy">/ 100</span></p>
+      </article>
+      <article class="third-bar">
         <div>
-          <img src=${jsonData[2].icon} alt="">
+          <img src=${jsonData[2].icon} alt=${jsonData[2].category}>
           <p>${jsonData[2].category}</p>
         </div>
-        <p>${jsonData[2].score} / 100</p>  
-      </div>
-      <div class="fourth-bar">
+        <p><span class="bold">${jsonData[2].score}</span> <span class="light-navy">/ 100</span></p>  
+      </article>
+      <article class="fourth-bar">
         <div>
-          <img src=${jsonData[3].icon} alt="">
+          <img src=${jsonData[3].icon} alt=${jsonData[3].category}>
           <p>${jsonData[3].category}</p>
         </div>
-        <p>${jsonData[3].score} / 100</p>  
-      </div>
+        <p><span class="bold">${jsonData[3].score}</span> <span class="light-navy">/ 100</span></p>  
+      </article>
     `
 }
 
